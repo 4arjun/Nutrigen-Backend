1b3d61870a9faae37aaab71a14102bd18bc52c83
4arjun <arjunajith440@gmail.com>
2025-01-14 23:32:11 +0530

    flask integration

--- allergens/urls.py
+++ allergens/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import detect_allergens
+from .views import upload_base64
 
 urlpatterns = [
     path("detect/", detect_allergens, name="detect_allergens"),
+    path("upload-base64",upload_base64,name="upload_base64")
 ]
