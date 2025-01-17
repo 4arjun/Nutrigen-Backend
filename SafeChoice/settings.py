5ed973165424fb56bfdc5f565d1cd95e281f74ce
krishnanx <abhishekkrishnan2006@gmail.com>
2025-01-17 11:57:32 +0530

    supabase connection & llm made dynamic

--- SafeChoice/settings.py
+++ SafeChoice/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,10 +83,15 @@ WSGI_APPLICATION = 'SafeChoice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        "USER" : os.getenv("user"),
+        "PASSWORD" : os.getenv("password"),
+        "HOST" : os.getenv("host"),
+        "PORT" : os.getenv("port"),
+        "DBNAME" : os.getenv("dbname"),
+            }
 }
+
 
 
 # Password validation
