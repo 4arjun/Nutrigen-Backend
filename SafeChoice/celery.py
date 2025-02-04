91c21d105f5280c52afcb6e4884a44250885f3e3
stefanbinoj <stefan.binoj.007@gmail.com>
2025-02-04 18:54:11 +0530

    removed boilerplate

--- SafeChoice/celery.py
+++ SafeChoice/celery.py
@@ -6,15 +6,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SafeChoice.settings')
 
 app = Celery('tasks')
 
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.conf.task_acks_late = True
-app.conf.task_reject_on_worker_lost = True
 
-# Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
