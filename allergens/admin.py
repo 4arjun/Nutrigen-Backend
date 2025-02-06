7ba6d6e5e223858535bc2c3e7dc14b13f380cb0a
stefanbinoj <stefan.binoj.007@gmail.com>
2025-02-06 07:55:06 +0530

    registered db

--- allergens/admin.py
+++ allergens/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from allergens.models import Users
+from .models import Users, History, Saved
 
+# Register your models here
 admin.site.register(Users)
+admin.site.register(History)
+admin.site.register(Saved)
