91358a3d272bccb360bf54661bcffae00553e491
stefanbinoj <stefan.binoj.007@gmail.com>
2025-02-06 07:35:15 +0530

    websocket connection using uviconr setup

--- allergens/consumers.py
+++ allergens/consumers.py
@@ -7,17 +7,15 @@ class MyWebSocketConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add("notifications",self.channel_name)
 
 
-    async def disconnect(self, close_code):
+    async def disconnect(self):
         await self.channel_layer.group_discard("notifications",self.channel_name)
 
     async def receive(self, text_data):
-        # Handle the message received from WebSocket
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name,
+            "notifications",
             {
                 'type': 'chat_message',
                 'message': message
@@ -25,10 +23,8 @@ class MyWebSocketConsumer(AsyncWebsocketConsumer):
         )
 
     async def chat_message(self, event):
-        # Handle the event sent to the group
         message = event['message']
 
-        # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message
         }))
--- allergens/urls.py
+++ allergens/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import upload_base64
+from .consumers import MyWebSocketConsumer
 
 urlpatterns = [
     path("upload-base64/",upload_base64,name="upload_base64")
+]
+
+websocket_urlpatterns = [
+    path('ws/chat/', MyWebSocketConsumer.as_asgi()),
 ]
